3093189ef0d5b397b87bcbeae604620768b0b38e
Ahmed Hathout <ahathout@tarkin.stuttgart.brandwatch.net>
2018-08-08 17:32:11 +0200

    Initial Commit

---
+++ src/main/java/com/brandwatch/internship/fetchandstoretweets/FetchAndStoreTweetsApplication.java
@@ -0,0 +1,12 @@
+package com.brandwatch.internship.fetchandstoretweets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchAndStoreTweetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FetchAndStoreTweetsApplication.class, args);
+    }
+}
