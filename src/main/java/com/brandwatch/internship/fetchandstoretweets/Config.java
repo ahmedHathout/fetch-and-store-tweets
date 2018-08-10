9933b5e9e2910f98b96ec8145e33d12e0cbebf1a
Ahmed Hathout <ahathout@brandwatch.com>
2018-08-10 09:47:53 +0200

    Add the Config file

--- src/main/java/com/brandwatch/internship/fetchandstoretweets/Config.java
+++ src/main/java/com/brandwatch/internship/fetchandstoretweets/Config.java
@@ -1,4 +1,21 @@
 package com.brandwatch.internship.fetchandstoretweets;
 
-public class Configuration {
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.social.twitter.api.Twitter;
+import org.springframework.social.twitter.api.impl.TwitterTemplate;
+
+@EnableScheduling
+@Configuration
+public class Config {
+
+    @Bean
+    public Twitter twitter(final @Value("${spring.social.twitter.appId}") String appId,
+                           final @Value("${spring.social.twitter.appSecret}") String appSecret) {
+
+        return new TwitterTemplate(appId, appSecret);
+    }
+
 }
