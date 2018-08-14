638aa1db0c3243d8efc929f45d4822b788eb5b96
Ahmed Hathout <ahathout@brandwatch.com>
2018-08-14 18:00:35 +0200

    Add a finder method

--- src/main/java/com/brandwatch/internship/fetchandstoretweets/repositories/MentionsRepository.java
+++ src/main/java/com/brandwatch/internship/fetchandstoretweets/repositories/MentionsRepository.java
@@ -3,6 +3,10 @@ package com.brandwatch.internship.fetchandstoretweets.repositories;
 import com.brandwatch.internship.fetchandstoretweets.entities.Mention;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface MentionsRepository extends MongoRepository<Mention, Long> {
+
+    List<Mention> findById_QueryId(long queryId);
 
 }
