aa02b67a5a9b7ae9776504b6b702c126257f0f3d
Ahmed Hathout <ahathout@brandwatch.com>
2018-08-14 17:57:52 +0200

    Add a setter for searchString

--- src/main/java/com/brandwatch/internship/fetchandstoretweets/entities/Query.java
+++ src/main/java/com/brandwatch/internship/fetchandstoretweets/entities/Query.java
@@ -2,11 +2,12 @@ package com.brandwatch.internship.fetchandstoretweets.entities;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.data.annotation.Id;
 
 public class Query {
 
     private final long id;
-    private final String searchString;
+    private String searchString;
 
     public Query(long id, String searchString) {
         this.id = id;
@@ -19,6 +20,10 @@ public class Query {
 
     public String getSearchString() {
         return searchString;
+    }
+
+    public void setSearchString(String searchString) {
+        this.searchString = searchString;
     }
 
     @Override
