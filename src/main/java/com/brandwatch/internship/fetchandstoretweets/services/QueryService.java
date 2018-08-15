99407cce7b4a3b21d11bb0b3f0cf111e74d743f7
Ahmed Hathout <ahathout@brandwatch.com>
2018-08-15 17:05:22 +0200

    Throw exceptions when needed

--- src/main/java/com/brandwatch/internship/fetchandstoretweets/services/QueryService.java
+++ src/main/java/com/brandwatch/internship/fetchandstoretweets/services/QueryService.java
@@ -3,7 +3,6 @@ package com.brandwatch.internship.fetchandstoretweets.services;
 import com.brandwatch.internship.fetchandstoretweets.entities.Query;
 import com.brandwatch.internship.fetchandstoretweets.repositories.QueryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.ParameterOutOfBoundsException;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
@@ -18,12 +17,10 @@ public class QueryService {
 
     @Autowired
     private QueryService(QueryRepository queryRepository) {
-
         try {
             this.topId = new AtomicLong(queryRepository.findFirstByOrderByIdDesc().getId());
         }
-        catch (ParameterOutOfBoundsException e) {
-            e.printStackTrace();
+        catch (NullPointerException e) {
             this.topId = new AtomicLong();
         }
 
@@ -38,43 +35,34 @@ public class QueryService {
     }
 
     public Query createQuery(String searchString) {
-        if (searchString.equals("")) {
-            return emptyQuery();
-        }
-        Query query = new Query(topId.incrementAndGet(), searchString);
+        if (searchString.equals(""))
+            throw new Query.EmptyQuerySearchStringException();
 
-        queryRepository.save(query);
-        return query;
+        return queryRepository.save(new Query(topId.incrementAndGet(), searchString));
     }
 
     public Query getQueryById(long id) {
         Query query = queryRepository.findOneById(id);
 
         if (query == null)
-            query = noSuchQuery();
+            throw new Query.NoSuchQueryException(id);
 
         return query;
     }
 
     public Query updateQuery(long id, String searchString) {
+        if (searchString.equals(""))
+            throw new Query.EmptyQuerySearchStringException();
+
         Query query = queryRepository.findOneById(id);
 
-        if (query != null) {
-            query.setSearchString(searchString);
-            queryRepository.save(query);
-        }
+        if (query == null)
+            throw new Query.NoSuchQueryException(id);
 
-        else
-            query = noSuchQuery();
+        query.setSearchString(searchString);
+        queryRepository.save(query);
 
         return query;
     }
 
-    private Query noSuchQuery() {
-        return new Query(-1L, "No such query bro :-)");
-    }
-
-    private Query emptyQuery() {
-        return new Query(-1L, "The searchString is empty!");
-    }
 }
