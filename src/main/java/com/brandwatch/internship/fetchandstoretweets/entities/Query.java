c0510e90c3cb8a0e77169481a04af0140df19e25
Ahmed Hathout <ahathout@brandwatch.com>
2018-08-15 17:02:56 +0200

    Add 2 exception classes

--- src/main/java/com/brandwatch/internship/fetchandstoretweets/entities/Query.java
+++ src/main/java/com/brandwatch/internship/fetchandstoretweets/entities/Query.java
@@ -2,9 +2,24 @@ package com.brandwatch.internship.fetchandstoretweets.entities;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.data.annotation.Id;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class Query {
+
+    @ResponseStatus(value=HttpStatus.NOT_FOUND, reason="No such Query")  // 404
+    public static class NoSuchQueryException extends RuntimeException {
+        public long id;
+
+        public NoSuchQueryException(long id) {
+            super();
+            this.id = id;
+        }
+    }
+
+    @ResponseStatus(value=HttpStatus.BAD_REQUEST, reason="searchString can not be empty")  // 400
+    public static class EmptyQuerySearchStringException extends RuntimeException { }
+
 
     private final long id;
     private String searchString;
