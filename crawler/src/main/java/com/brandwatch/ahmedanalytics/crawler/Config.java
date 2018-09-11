71669fef6c7b6c5ab3ff1e6330275dedee9a46c9
Ahmed Hathout <ahathout@brandwatch.com>
2018-09-11 10:57:06 +0200

    Add KafkaProducer bean to crawler/Config

--- crawler/src/main/java/com/brandwatch/ahmedanalytics/crawler/Config.java
+++ crawler/src/main/java/com/brandwatch/ahmedanalytics/crawler/Config.java
@@ -1,11 +1,16 @@
 package com.brandwatch.ahmedanalytics.crawler;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
+
+import java.util.Properties;
 
 @EnableScheduling
 @Configuration
@@ -16,6 +21,16 @@ public class Config {
                            final @Value("${spring.social.twitter.appSecret}") String appSecret) {
 
         return new TwitterTemplate(appId, appSecret);
+    }
+
+    @Bean
+    public KafkaProducer<String, String> mentionProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+        return new KafkaProducer<>(props);
     }
 
 }
