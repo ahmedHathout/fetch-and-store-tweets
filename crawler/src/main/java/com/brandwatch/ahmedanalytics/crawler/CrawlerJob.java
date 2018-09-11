dca59b90e5e250f88552063ba28f63bc38bbce5c
Ahmed Hathout <ahathout@brandwatch.com>
2018-09-11 08:56:32 +0200

    Move and adapt CrawlerJob for Kafka

--- crawler/src/main/java/com/brandwatch/ahmedanalytics/crawler/CrawlerJob.java
+++ crawler/src/main/java/com/brandwatch/ahmedanalytics/crawler/CrawlerJob.java
@@ -1,9 +1,12 @@
-package com.brandwatch.ahmedanalytics.crawler.job;
+package com.brandwatch.ahmedanalytics.crawler;
 
 import com.brandwatch.ahmedanalytics.common.entities.Mention;
 import com.brandwatch.ahmedanalytics.common.entities.Query;
-import com.brandwatch.ahmedanalytics.common.services.MentionService;
 import com.brandwatch.ahmedanalytics.common.services.QueryService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.social.twitter.api.Tweet;
 import org.springframework.social.twitter.api.Twitter;
@@ -14,24 +17,34 @@ import java.util.stream.Collectors;
 
 @Component
 public class CrawlerJob {
+    private static final ObjectMapper mapper = new ObjectMapper();
 
     private final Twitter twitter;
-    private MentionService mentionService;
+    private final Producer<String, String> producer;
+
     private QueryService queryService;
 
-    public CrawlerJob(Twitter twitter, MentionService mentionService, QueryService queryService) {
+    public CrawlerJob(Twitter twitter, Producer<String, String> producer, QueryService queryService) {
         this.twitter = twitter;
-        this.mentionService = mentionService;
+        this.producer = producer;
         this.queryService = queryService;
     }
 
     @Scheduled(fixedRate = 60000)
     public void crawl() {
         List<Query> queries = queryService.findAll();
-
         for (Query query : queries) {
             List<Tweet> tweets = twitter.searchOperations().search(query.getSearchString()).getTweets();
-            mentionService.saveMentions(tweetsToMentions(tweets, query.getId()));
+            List<Mention> mentions = tweetsToMentions(tweets, query.getId());
+
+            mentions.forEach(mention -> {
+                try {
+                    producer.send(new ProducerRecord<>("mention",
+                            mapper.writeValueAsString(mention)));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            });
         }
     }
 
