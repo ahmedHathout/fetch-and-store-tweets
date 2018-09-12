c5a5f0f4499f5b55241802242ac6818b1a2f5699
Ahmed Hathout <ahathout@brandwatch.com>
2018-09-12 12:53:26 +0200

    StringSerilizer -> JsonSerializer & use external bootstrap config

--- crawler/src/main/java/com/brandwatch/ahmedanalytics/crawler/Config.java
+++ crawler/src/main/java/com/brandwatch/ahmedanalytics/crawler/Config.java
@@ -1,19 +1,21 @@
 package com.brandwatch.ahmedanalytics.crawler;
 
+import com.brandwatch.ahmedanalytics.common.entities.Mention;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.social.twitter.api.Twitter;
 import org.springframework.social.twitter.api.impl.TwitterTemplate;
 
 import java.util.Properties;
 
-@EnableScheduling
 @Configuration
+@EnableScheduling
 public class Config {
 
     @Bean
@@ -24,11 +26,11 @@ public class Config {
     }
 
     @Bean
-    public KafkaProducer<String, String> kafkaProducer() {
+    public KafkaProducer<String, Mention> kafkaProducer(@Value("${spring.kafka.producer.bootstrap-servers}") String bootstrapServers) {
         Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "kafka:9092");
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
 
         return new KafkaProducer<>(props);
     }
